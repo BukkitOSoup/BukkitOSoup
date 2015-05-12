17d3c168d6829186736366b298f63a054951bab7
BukkitOSoup <bukkitosoup@gmail.com>
2015-05-12 01:10:50 -0400

    Still trying to get login messages to work

--- src/com/ogxclaw/bukkitosoup/listeners/BukkitOSoupPlayerListener.java
+++ src/com/ogxclaw/bukkitosoup/listeners/BukkitOSoupPlayerListener.java
@@ -41,63 +41,55 @@ public class BukkitOSoupPlayerListener implements Listener {
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onPlayerJoin(PlayerJoinEvent event){
 		Player player = event.getPlayer();
-		/*String colorCode;
-		String rank = plugin.getConfig().getString(player.getName() + ".rank");
-		int level = plugin.getConfig().getInt(player.getName() + ".level");
-		if(rank == null){
-			plugin.getConfig().set(player.getName() + ".rank", "Guest");
+		String colorCode;
+		String rank = plugin.getPlayerData().getString(player.getName() + ".rank");
+		int level = plugin.getPlayerData().getInt(player.getName() + ".level");
+		if(rank == ""){
+			plugin.getPlayerData().set(player.getName() + ".rank", "Guest");
 		}
 		if(level == 0){
-			plugin.getConfig().set(player.getName() + ".level", 10);
+			plugin.getPlayerData().set(player.getName() + ".level", 10);
 		}
-		plugin.getServer().dispatchCommand(plugin.getServer().getConsoleSender(), "pex user " + player.getName() + " group set " + rank);
-		if(rank == "Guest"){
-			colorCode = "7";
-		}else if(rank == "Builder"){
-			colorCode = "a";
-		}else if(rank == "AdvBuilder"){
-			colorCode = "c";
-		}else if(rank == "Moderator"){
-			colorCode = "b";
-		}else if(rank == "Admin"){
-			colorCode = "2";
-		}else if(rank == "Owner"){
-			colorCode = "5";
-		}else{
-			colorCode = "7";
-		}*/
+		colorCode = getPlayerColor(player);
+		//plugin.getServer().dispatchCommand(plugin.getServer().getConsoleSender(), "pex user " + player.getName() + " group set " + rank);
+		plugin.savePlayerData();
 		BaseCommand.sendDirectedMessage(player, "Welcome to \u00a7eBowlOCraft\u00a7f.");
 		BaseCommand.sendDirectedMessage(player, "Use \u00a73/rules \u00a7ffor rules and \u00a73/help\u00a7f for help!");
-		BaseCommand.sendDirectedMessage(player, "Use \u00a73/staff\u00a7f for a list of staff!");
-		plugin.saveConfig();
-		event.setJoinMessage("\u00a72[+] \u00a7ePlayer \u00a77" + player.getDisplayName() + " \u00a7econnected");
+		BaseCommand.sendDirectedMessage(player, "Please report all bugs to \u00a73Ogxclaw\u00a7f!");
+		//plugin.saveConfig();
+		event.setJoinMessage("\u00a72[+] \u00a7ePlayer \u00a7" + colorCode + player.getDisplayName() + " \u00a7econnected");
 	}
 	
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onPlayerLeave(PlayerQuitEvent event){
 		Player player = event.getPlayer();
-		/*String colorCode;
-		String rank = plugin.getConfig().getString(player.getName() + ".rank");
-		if(rank == "Guest"){
-			colorCode = "7";
-		}else if(rank == "Builder"){
-			colorCode = "a";
-		}else if(rank == "AdvBuilder"){
-			colorCode = "c";
-		}else if(rank == "Moderator"){
-			colorCode = "b";
-		}else if(rank == "Admin"){
-			colorCode = "2";
-		}else if(rank == "Owner"){
-			colorCode = "5";
-		}else{
-			colorCode = "7";
-		}*/
-		plugin.saveConfig();
+		String colorCode;
+		//String rank = plugin.getPlayerData().getString(player.getName() + ".rank");
+		colorCode = getPlayerColor(player);
+		plugin.savePlayerData(); 
 		if(player.isBanned()){
 			event.setQuitMessage("\u00a74[-] \u00a7ePlayer \u00a70" + player.getDisplayName() + " \u00a7edisconnected");
 		}else{
-			event.setQuitMessage("\u00a74[-] \u00a7ePlayer \u00a77" + player.getDisplayName() + " \u00a7edisconnected");
+			event.setQuitMessage("\u00a74[-] \u00a7ePlayer \u00a7" + colorCode + player.getDisplayName() + " \u00a7edisconnected");
+		}
+	}
+	
+	private String getPlayerColor(Player player){
+		String rank = plugin.getPlayerData().getString(player.getName() + ".rank");
+		if(rank == "Guest"){
+			return "7";
+		}else if(rank == "Builder"){
+			return "a";
+		}else if(rank == "AdvBuilder"){
+			return "c";
+		}else if(rank == "Moderator"){
+			return "b";
+		}else if(rank == "Admin"){
+			return "2";
+		}else if(rank == "Owner"){
+			return "5";
+		}else{
+			return "7";
 		}
 	}
 
